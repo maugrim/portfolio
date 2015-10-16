3dcd5c5158f066d6f32a4f3be323565d4d325cdb
maugrim <marshall@quander.me>
2015-10-15 21:38:59 -0700

    Upgrade to Clojure 1.7

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.3"]
                  [clj-http "0.7.7" :exclusions [crouton]]])
