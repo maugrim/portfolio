d3a91aa89a73f7ca664efa585b659f2cd381217f
Marshall Quander <marshall@quander.me>
2017-12-13 12:01:43 -0800

    Switch to IEX API for quotes

--- src/portfolio/quotes.clj
+++ src/portfolio/quotes.clj
@@ -3,23 +3,22 @@
             [clojure.data.json :as json]
             [clojure.string :as string]))
 
-(def api-url "http://query.yahooapis.com/v1/public/yql")
+(def api-url "https://api.iextrading.com/1.0/stock/market/batch")
 
 (defn api-call [params]
   (http-client/get api-url {:query-params params}))
 
 (defn fetch-json [symbols]
-  (let [symbol-list (string/join "," (map (partial format "\"%s\"") symbols))]
-    (api-call {:q (format "select * from yahoo.finance.quotes where symbol in (%s)" symbol-list)
-               :env "http://datatables.org/alltables.env"
-               :format "json"})))
+  (let [symbol-list (string/join "," (map name symbols))]
+    (api-call {:symbols symbol-list
+               :types "quote"
+               :last 1})))
 
 (defn fetch-quotes [symbols]
   (-> (:body (fetch-json symbols))
-      (json/read-str :key-fn keyword)
-      (get-in [:query :results :quote])))
+      (json/read-str :key-fn keyword)))
 
 (defn fetch-prices [symbols]
   (->> (fetch-quotes symbols)
-       (map (juxt :Symbol #(Float/parseFloat (:Ask %))))
+       (map (fn [[k v]] [k (get-in v [:quote :latestPrice])]))
        (into {})))
